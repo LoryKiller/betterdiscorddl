5d16f9bd6efc8e4cde970e3232c8b3922dc46626
Lorenzo <Lorenzo.scordella08@mail.com>
2025-02-11 16:30:34 +0100

    v0.1

--- script.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-set downloadpathdiscorddl=%1
-cd %downloadpathdiscorddl%
-:: prova>prova.txt
-
-pnpm install && pnpm build && echo starting injecting && pnpm inject && taskkill /f /im discord.exe && C:\Users\loren\AppData\Local\Discord\Update.exe --processStart Discord.exe && cd .. && timeout /T 3 && rmdir /s /q %downloadpathdiscorddl%
