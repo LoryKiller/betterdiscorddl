c7254d4ee620df6c74c4ca163d3f5fecb382df7b
Lorenzo <Lorenzo.scordella08@mail.com>
2025-02-11 16:40:58 +0100

    added progress bar

--- betterdiscorddl.py
+++ betterdiscorddl.py
@@ -2,19 +2,29 @@
 import requests
 import os
 import glob
+import tqdm
 url = "https://github.com/BetterDiscord/Installer/releases/latest/download/BetterDiscord-Windows.exe"
 userprofile = os.getenv("USERPROFILE")
-temp = os.getenv('userprofile') + r"\ " +"downloads" + r"\ "
+temp = os.getenv('temp') + "\\"
 temp = str(temp)
 temp = temp.replace(" ","")
 temp = temp.replace(os.sep,"/")
 path=temp+"BetterDiscord-Windows.exe"
 
 
+response = requests.get(url, stream=True)
 
+total_size = int(response.headers.get("content-length", 0))
+block_size = 1024
 
-response = requests.get(url)
-open(path, "wb").write(response.content)
+with tqdm.tqdm(total=total_size, unit="B", unit_scale=True) as progress_bar:
+    with open(path, "wb") as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+
+if total_size != 0 and progress_bar.n != total_size:
+    raise RuntimeError("Could not download file")
 
 while True:
     if glob.glob(path):
